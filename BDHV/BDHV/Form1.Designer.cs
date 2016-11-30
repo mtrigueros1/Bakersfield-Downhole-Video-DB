14a88710b8168a642d3fa013c57c8d54599e73e1
beane <beane@DESKTOP-F27COR6>
2016-11-30 15:36:41 -0800

    Project Added

---
+++ BDHV/BDHV/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace BDHV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.year1 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dd1_day = new System.Windows.Forms.ComboBox();
+            this.dd1_month = new System.Windows.Forms.ComboBox();
+            this.dd1_month2 = new System.Windows.Forms.ComboBox();
+            this.dd1_day2 = new System.Windows.Forms.ComboBox();
+            this.year2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 106);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(700, 366);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DarkGray;
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tabPage1.Controls.Add(this.dd1_month2);
+            this.tabPage1.Controls.Add(this.dd1_day2);
+            this.tabPage1.Controls.Add(this.year2);
+            this.tabPage1.Controls.Add(this.dd1_month);
+            this.tabPage1.Controls.Add(this.dd1_day);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.year1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(692, 340);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Reports";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(692, 340);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(249, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // year1
+            // 
+            this.year1.Location = new System.Drawing.Point(260, 33);
+            this.year1.Name = "year1";
+            this.year1.Size = new System.Drawing.Size(100, 20);
+            this.year1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 193);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dd1_day
+            // 
+            this.dd1_day.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dd1_day.FormattingEnabled = true;
+            this.dd1_day.Location = new System.Drawing.Point(6, 32);
+            this.dd1_day.Name = "dd1_day";
+            this.dd1_day.Size = new System.Drawing.Size(121, 21);
+            this.dd1_day.TabIndex = 8;
+            // 
+            // dd1_month
+            // 
+            this.dd1_month.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dd1_month.FormattingEnabled = true;
+            this.dd1_month.Location = new System.Drawing.Point(133, 32);
+            this.dd1_month.Name = "dd1_month";
+            this.dd1_month.Size = new System.Drawing.Size(121, 21);
+            this.dd1_month.TabIndex = 9;
+            // 
+            // dd1_month2
+            // 
+            this.dd1_month2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dd1_month2.FormattingEnabled = true;
+            this.dd1_month2.Location = new System.Drawing.Point(133, 91);
+            this.dd1_month2.Name = "dd1_month2";
+            this.dd1_month2.Size = new System.Drawing.Size(121, 21);
+            this.dd1_month2.TabIndex = 12;
+            // 
+            // dd1_day2
+            // 
+            this.dd1_day2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dd1_day2.FormattingEnabled = true;
+            this.dd1_day2.Location = new System.Drawing.Point(6, 91);
+            this.dd1_day2.Name = "dd1_day2";
+            this.dd1_day2.Size = new System.Drawing.Size(121, 21);
+            this.dd1_day2.TabIndex = 11;
+            // 
+            // year2
+            // 
+            this.year2.Location = new System.Drawing.Point(260, 92);
+            this.year2.Name = "year2";
+            this.year2.Size = new System.Drawing.Size(100, 20);
+            this.year2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(724, 484);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bakersfield Downhole Video";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox year1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox dd1_month;
+        private System.Windows.Forms.ComboBox dd1_day;
+        private System.Windows.Forms.ComboBox dd1_month2;
+        private System.Windows.Forms.ComboBox dd1_day2;
+        private System.Windows.Forms.TextBox year2;
+    }
+}
+
---
+++ BDHV/BDHV/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Globalization;
+
+namespace BDHV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Add_Date();
+            dd1_day.SelectedIndex = 0;
+            dd1_day2.SelectedIndex = 0;
+            Add_Month();
+            dd1_month.SelectedIndex = 0;
+            dd1_month2.SelectedIndex = 0;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void Add_Date()
+        {
+            dd1_day.Items.Add("Day");
+            dd1_day2.Items.Add("Day");
+
+            for (int j = 0; j < 31; j++)
+            {
+                var newOption = Convert.ToString(j + 1);
+                dd1_day.Items.Add(newOption);
+                dd1_day2.Items.Add(newOption);
+            }
+        }
+        private void Add_Month()
+        {
+            dd1_month.Items.Add("Month");
+            dd1_month2.Items.Add("Month");
+
+            for (int j = 0; j < 12; j++)
+            {
+                var newOption = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(j + 1);
+                dd1_month.Items.Add(newOption);
+                dd1_month2.Items.Add(newOption);
+
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string oradb = "DATA SOURCE=delphi.cs.csubak.edu:1521/dbs01.cs.csubak;USER ID=cs3420; PASSWORD=c3m4p2s";
+            string s = dd1_month.SelectedIndex + "-" + dd1_day.SelectedIndex + "-" + year1.Text;
+            Console.WriteLine(s);
+            int correct = 0;
+            DateTime dt;
+            if (!DateTime.TryParse(s, CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
+            {
+                correct += 0;
+                Console.WriteLine("Date 1 wrong!");
+            }
+            else
+            {
+                s = dd1_day.SelectedIndex + "-" + dd1_month.SelectedIndex + "-" + year1.Text;
+                correct += 1;
+            }
+
+            string s2 = dd1_month2.SelectedIndex + "-" + dd1_day2.SelectedIndex + "-" + year2.Text;
+            DateTime dt2;
+            if (!DateTime.TryParse(s2, CultureInfo.InvariantCulture, DateTimeStyles.None, out dt2))
+            {
+                correct += 0;
+                Console.WriteLine("Date 2 wrong!");
+            }
+            else
+            {
+                s2 = dd1_day2.SelectedIndex + "-" + dd1_month2.SelectedIndex + "-" + year2.Text;
+                correct += 1;
+            }
+            if (correct == 2)
+            {
+
+                string test = null;
+
+                try
+                {
+                    OracleConnection conn = new OracleConnection(oradb);
+                    var cmd = new OracleCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "CAM_SumHours";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("hrsum", OracleDbType.Decimal);
+                    cmd.Parameters["hrsum"].Direction = ParameterDirection.ReturnValue;
+                    cmd.Parameters.Add("start_date", s);
+                    cmd.Parameters.Add("end_date", s2);
+                    cmd.Parameters["end_date"].Direction = ParameterDirection.Input;
+                    cmd.Parameters["start_date"].Direction = ParameterDirection.Input;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Connection succesful!");
+                    Console.WriteLine(cmd.Parameters["hrsum"].Value);
+                    var rt = cmd.Parameters["hrsum"].Value;
+                    test = Convert.ToString(rt);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error:{0}", ex.Message);
+                }
+                label1.Text = test;
+            } else {
+                Console.WriteLine("Test was less than 2!");
+            }
+        }
+    }
+}
